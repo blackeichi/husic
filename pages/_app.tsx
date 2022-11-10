9a2e58734c20d8f18ed7eb2fd375a8eb21fff4ca
blackeichi <87626378+blackeichi@users.noreply.github.com>
2022-11-10 14:37:55 +0900

    2022.11.10

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import ReactPlayer from "react-player";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="bg-black w-full h-screen">
+      <div className="w-full p-3 bg-black fixed flex justify-between items-center">
+        <h1 className="text-white font-Cafe24Shiningstar text-6xl">Husic</h1>
+      </div>
+      <Component {...pageProps} />
+    </div>
+  );
 }
