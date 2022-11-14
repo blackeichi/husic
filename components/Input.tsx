1de961ce6929c5640c3feeac1d880e3dea274dad
blackeichi <87626378+blackeichi@users.noreply.github.com>
2022-11-14 18:22:05 +0900

    22.11.14 Enter pages

--- components/Input.tsx
+++ components/Input.tsx
@@ -3,8 +3,24 @@ import type { UseFormRegisterReturn } from "react-hook-form";
 type InputProps = {
   label: string;
   name: string;
-  kind?: "Join" | "Login";
+  kind?: "text";
   type: string;
   register: UseFormRegisterReturn;
   required: boolean;
+};
+
+export const Input = ({
+  label,
+  name,
+  kind = "text",
+  type,
+  register,
+  required,
+}: InputProps) => {
+  return (
+    <div className="">
+      <label>{label}</label>
+      <input></input>
+    </div>
+  );
 };
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,7 +1,28 @@
 import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import { Input } from "../components/Input";
+
+type EnterForm = {
+  username?: string;
+  password?: string;
+  confirmPw?: string;
+};
 
 export default function Enter() {
   const router = useRouter();
   const { form } = router.query;
-  return <div className="w-full h-screen flex flex-col"></div>;
+  const { register, handleSubmit, reset } = useForm<EnterForm>();
+  return (
+    <div className="w-full h-screen flex flex-col">
+      <Input
+        label={"User Name"}
+        name={"username"}
+        type={"text"}
+        register={register("username", {
+          required: true,
+        })}
+        required
+      />
+    </div>
+  );
 }
